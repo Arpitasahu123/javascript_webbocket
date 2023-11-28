0a43c183cdc435137562532056ac94a66d5ed1e6
Arpitasahu123 <151999996+Arpitasahu123@users.noreply.github.com>
2023-11-28 10:19:05 +0000

    done

---
+++ 01-basics/test.js
@@ -0,0 +1,9 @@
+console.log("namaste dunia");
+console.log("hello i am arpita");
+/*
+1. Javascript is a high level scripting language to use on developing web and mobile application efficiency.
+2. we used javascript on client side as well as server side
+3.in javascript we do logical implentation in web pages.
+4. in today era use esb version of javascript ,as it is latest one.
+*esb- echma script b(vanilla javascript)
+*/
